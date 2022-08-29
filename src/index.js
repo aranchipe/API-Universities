d3c0680b877526339c32f28cd30a4dac68ba6340
Lucas <aranchipe98@gmail.com>
2022-08-29 13:59:44 -0300

    endpoint listar universidades

--- src/index.js
+++ src/index.js
@@ -16,8 +16,9 @@ app.use(express.json())
 app.use(cors())
 app.use(rotas)
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(3000 || process.env.PORT)
-    })
-    .catch((err) => console.log(err))
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+app.listen(3000 || process.env.PORT)
